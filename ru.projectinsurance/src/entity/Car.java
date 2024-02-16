4ffcf8305f5b05512c8e11c0181c3224884a381c
BasttUK <157982398+BasttUK@users.noreply.github.com>
2024-02-16 22:19:28 +0300

    добавил саморасширяющийся массив прописал сущность "car" (не до конца, возникли вопросы)

--- ru.projectinsurance/src/entity/Car.java
+++ ru.projectinsurance/src/entity/Car.java
@@ -1,4 +1,41 @@
 package entity;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class Car {
+    private int id;
+    private String carMake:
+    private String color;
+    private String carPlate;
+
+    public Car (int id, String carMake, String color, String carPlate) {
+        this.id = id;
+        this.carMake = carMake;
+        this.color = color;
+        this.carPlate = carPlate;
+    }
+    public int getId () {
+        return id;
+    }
+    public String getCarMake () {
+        return carMake;
+    }
+    public String getColor () {
+        return color;
+    }
+    public String getCarPlate () {
+        return carPlate;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
+
+    }
+
+
+
 }
